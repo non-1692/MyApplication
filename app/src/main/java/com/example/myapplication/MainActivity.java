64198f2e401fabd8c403e48bf017bcf3d397f201
Noe Rojas <non.rojas.flores@gmail.com>
2019-05-07 21:52:31 -0500

    Add metodo suma simple

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -2,16 +2,27 @@ package com.example.myapplication;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 
 public class MainActivity extends AppCompatActivity {
 
-    private int dafault = 0;
+    private static final String TAG = "MainActivityApp";
+    private int numero = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+       int a = suma(numero, 12);
+        Log.d(TAG, "suma: " + a);
 
     }
+
+    private int suma(int numero, int i) {
+        int r = numero + i;
+        return r;
+    }
+
+
 }
